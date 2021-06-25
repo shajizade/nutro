dd34d6360adac052c5f8444489e74839ee995dca
Saeed <s.hajizadeh@gmail.com>
2021-06-25 11:36:44 +0430

    lot of changes

--- newui/src/views/dashboard/Dashboard.js
+++ newui/src/views/dashboard/Dashboard.js
@@ -12,11 +12,14 @@ import {
   CRow,
   CCallout
 } from "@coreui/react";
+import WidgetsDropdown from "../widgets/WidgetsDropdown";
 
+/*
 const WidgetsDropdown = lazy(() =>
   ('../widgets/WidgetsDropdown.js'))
 const WidgetsBrand = lazy(() =>
   ('../widgets/WidgetsBrand.js'))
+ */
 
 const Dashboard = () => {
   return (
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,19 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
-import { Route } from "react-router";
+import React, {Suspense} from "react";
+import {BrowserRouter, Switch} from "react-router-dom";
+import {Route} from "react-router";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "./app.scss";
 import Layout from "./components/layout";
 import AppProvider from "./context";
+import Dashboard from "./pages/dashboard";
+import Register from "./pages/register";
+import Login from "./pages/login";
+
+/*
 const Dashboard = React.lazy(() => import("./pages/dashboard"));
 const Register = React.lazy(() => import("./pages/register"));
 const Login = React.lazy(() => import("./pages/login"));
+ */
 
 function App() {
     return (
